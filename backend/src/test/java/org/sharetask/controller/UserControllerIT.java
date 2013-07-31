8c6aa28239305e3d27b8138f89256fcb55a9b903
Michal Bocek <michal.bocek@gmail.com>
2013-07-31 14:08:46 +0200

    fixed test

--- backend/src/test/java/org/sharetask/controller/UserControllerIT.java
+++ backend/src/test/java/org/sharetask/controller/UserControllerIT.java
@@ -40,7 +40,7 @@ public class UserControllerIT extends IntegrationTest {
 
 	private static final String TASK_PATH = "/user";
     private static final String URL_USER = BASE_URL + TASK_PATH;
- 
+
     @Test
     public void testCreateUser() throws IOException {
         //given
@@ -50,17 +50,17 @@ public class UserControllerIT extends IntegrationTest {
         		                                     "\"name\":\"Integration\"," +
         		                                     "\"surName\":\"Test\"}");
         httpPost.setEntity(httpEntity);
-        
+
         //when
         final HttpResponse response = getClient().execute(httpPost);
- 
+
         //then
         Assert.assertEquals(HttpStatus.OK.value(), response.getStatusLine().getStatusCode());
         final String responseData = EntityUtils.toString(response.getEntity());
         Assert.assertTrue(responseData.contains("\"username\":\"it@shareta.sk\""));
     }
-    
-    
+
+
     @Test
     public void testUpdateUser() throws IOException {
         //given
@@ -70,25 +70,25 @@ public class UserControllerIT extends IntegrationTest {
         		                                     "\"name\":\"Integration\"," +
         		                                     "\"surName\":\"Test\"}");
         httpPut.setEntity(httpEntity);
-        
+
         //when
         final HttpResponse response = getClient().execute(httpPut);
- 
+
         //then
         Assert.assertEquals(HttpStatus.OK.value(), response.getStatusLine().getStatusCode());
         final String responseData = EntityUtils.toString(response.getEntity());
         Assert.assertTrue(responseData.contains("\"name\":\"Integration\""));
     }
-    
+
     @Test
     public void testReadUser() throws IOException {
         //given
-        final HttpGet httpGet = new HttpGet(URL_USER + "/dev1@shareta.sk");
+        final HttpGet httpGet = new HttpGet(URL_USER + "?username=dev1%40shareta.sk");
         httpGet.addHeader(new BasicHeader("Content-Type", "application/json"));
-        
+
         //when
         final HttpResponse response = getClient().execute(httpGet);
- 
+
         //then
         Assert.assertEquals(HttpStatus.OK.value(), response.getStatusLine().getStatusCode());
         final String responseData = EntityUtils.toString(response.getEntity());
