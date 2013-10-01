a54595de69835ec69c74b485c8c35dbb2cf5eb56
Michal Bocek <michal.bocek@gmail.com>
2013-10-01 22:11:08 +0200

    fixed #53

--- backend/src/main/java/org/sharetask/service/TaskServiceImpl.java
+++ backend/src/main/java/org/sharetask/service/TaskServiceImpl.java
@@ -134,7 +134,7 @@ public class TaskServiceImpl implements TaskService {
 	 */
 	@Override
 	@Transactional
-	@PreAuthorize("isAuthenticated() and hasPermission(#task.id, 'isTaskAssignee')")
+	@PreAuthorize(Constants.PERMISSION_TASK_ASSIGNEE_OR_CREATOR)
 	public TaskDTO update(final TaskDTO task) {
 		final Task entity = taskRepository.findOne(task.getId());
 		DTOConverter.convert(task, entity);
