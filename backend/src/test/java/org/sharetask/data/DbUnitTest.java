e28bba60112a587d94775ede7188cd9d3aa77343
Michal Bocek <michal.bocek@gmail.com>
2014-05-02 15:28:43 +0200

    fixed problem with mail sender bean loading in dev env

--- backend/src/test/java/org/sharetask/data/DbUnitTest.java
+++ backend/src/test/java/org/sharetask/data/DbUnitTest.java
@@ -56,7 +56,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({ "classpath:spring/template.xml", "classpath:spring/mail.xml", "classpath:spring/transaction.xml",
-		"classpath:spring/security.xml", "classpath:spring/applicationConfig.xml", "classpath:spring/test-mail.xml" })
+		"classpath:spring/security.xml", "classpath:spring/applicationConfig.xml" })
 public class DbUnitTest {
 
 	@PersistenceContext(name = "entityManagerFactory")
