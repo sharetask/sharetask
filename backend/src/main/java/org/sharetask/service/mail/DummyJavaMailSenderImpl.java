c49e7c1ca68c5291b17bec85d0b7f58f56564b62
Michal Bocek <michal.bocek@gmail.com>
2015-02-03 22:42:56 +0100

    upgrade to new libraries version

--- backend/src/main/java/org/sharetask/service/mail/DummyJavaMailSenderImpl.java
+++ backend/src/main/java/org/sharetask/service/mail/DummyJavaMailSenderImpl.java
@@ -25,8 +25,8 @@ import java.util.List;
 
 import javax.mail.internet.MimeMessage;
 
-import org.apache.commons.lang.builder.ToStringBuilder;
-import org.apache.commons.lang.builder.ToStringStyle;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
